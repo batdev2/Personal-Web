b36dabd84cee5c4f0a02b8994c07e48be3a65f90
batdev2 <razvan.tetulea10@gmail.com>
2024-05-09 15:58:20 +0300

    New Jquery Code in index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+$("#firstProject").on("click", function() {
+  location.href = "https://batdev2.github.io/tindog-website/";
+});
+
+$("#secondProject").on("click", function() {
+    location.href = "https://batdev2.github.io/move-it/";
+});
+
+$("#navProjects").on("click", function() {
+    location.href = "#projects";
+});
+
+$("#navContact").on("click", function() {
+    location.href = "#contact";
+});
